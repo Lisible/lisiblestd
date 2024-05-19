bd22b018c841cddf4eb8e1dae48eb14eef850523
Clement Sibille <clements+git@lisible.xyz>
2024-05-20 00:47:45 +0900

    Add bitset module

---
+++ src/lisiblestd/bitset.h
@@ -0,0 +1,13 @@
+#ifndef LSTD_BITSET_H
+#define LSTD_BITSET_H
+
+#include <limits.h>
+
+#define BITMASK(b) (1 << ((b) % CHAR_BIT))
+#define BITSLOT(b) ((b) / CHAR_BIT)
+#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
+#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
+#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
+#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
+
+#endif // LSTD_BITSET_H
